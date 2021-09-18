f216824adaba88fa0c3dd5b12d26ca1e29279703
Kuznetsov Ilya Alexandrovich <kuznetsov.ilya.alexandrovich@gmail.com>
2021-09-18 20:15:06 +0300

    Task 3 is done

---
+++ project/automaton_tools.py
@@ -0,0 +1,95 @@
+from typing import Set
+
+import networkx as nx
+from pyformlang.finite_automaton import \
+    DeterministicFiniteAutomaton, \
+    EpsilonNFA, NondeterministicFiniteAutomaton
+from pyformlang.regular_expression import Regex
+
+__all__ = ["get_min_dfa", "get_nfa"]
+
+
+def get_min_dfa(regex: str) -> DeterministicFiniteAutomaton:
+    """
+    Based on a regular expression given as a string, builds an Deterministic Finite Automaton.
+
+    Parameters
+    ----------
+    regex: str
+        The string representation of a regular expression
+
+    Returns
+    -------
+    DeterministicFiniteAutomaton
+        Deterministic Finite Automaton equivalent to a given regular expression as a string
+
+    Raises
+    ------
+    MisformedRegexError
+        If given as string regular expression has an irregular format
+    """
+
+    re = Regex(regex)
+    e_nfa = re.to_epsilon_nfa()
+    min_dfa = e_nfa.minimize()
+
+    return min_dfa
+
+
+def get_nfa(graph: nx.MultiDiGraph, start_nodes: Set[int] = None, final_nodes: Set[int] = None) \
+        -> NondeterministicFiniteAutomaton:
+    """
+    Generates an Epsilon Nondeterministic Finite Automaton for a specified graph and start or end nodes.
+
+    If start_nodes and final_nodes are not specified, all nodes are considered start and end.
+
+    Parameters
+    ----------
+    graph: nx.MultiDiGraph
+        Graph to generating an Epsilon Nondeterministic Finite Automaton from it
+    start_nodes: Set[int], default = None
+        Set of start nodes to configure Epsilon Nondeterministic Finite Automaton
+    final_nodes: Set[int], default = None
+        Set of final nodes to configure Epsilon Nondeterministic Finite Automaton
+
+    Returns
+    -------
+    EpsilonNFA
+        Epsilon Nondeterministic Finite Automaton equivalent to a specified graph
+
+    Raises
+    ------
+    ValueError
+        If number of non-existent in the specified graph node is used
+    """
+
+    nfa = NondeterministicFiniteAutomaton()
+
+    for node_from, node_to in graph.edges():
+        edge_label = graph.get_edge_data(node_from, node_to)[0]["label"]
+        nfa.add_transition(node_from, edge_label, node_to)
+
+    if not start_nodes and not final_nodes:
+        for state in nfa.states:
+            nfa.add_start_state(state)
+            nfa.add_final_state(state)
+
+    if start_nodes:
+        for start_node in start_nodes:
+            if start_node not in range(graph.number_of_nodes()):
+                raise ValueError(f"Node {start_node} does not exists in specified graph")
+
+        for node in start_nodes:
+            state = list(nfa.states)[node]
+            nfa.add_start_state(state)
+
+    if final_nodes:
+        for final_node in final_nodes:
+            if final_node not in range(graph.number_of_nodes()):
+                raise ValueError(f"Node {final_node} does not exists in specified graph")
+
+        for node in final_nodes:
+            state = list(nfa.states)[node]
+            nfa.add_final_state(state)
+
+    return nfa
---
+++ tests/test_automaton_tools.py
@@ -0,0 +1,171 @@
+from typing import Iterable
+
+import pytest
+from pyformlang.finite_automaton import Symbol, State, DeterministicFiniteAutomaton, NondeterministicFiniteAutomaton
+from pyformlang.regular_expression import MisformedRegexError
+
+from project import get_two_cycles, Graph
+from project.automaton_tools import *
+
+
+def test_wrong_regex() -> None:
+    with pytest.raises(MisformedRegexError):
+        get_min_dfa("*|wrong_regex|*")
+
+
+def test_dfa() -> None:
+    min_dfa = get_min_dfa("i* l* y* a* | 1901")
+
+    assert min_dfa.is_deterministic()
+
+
+def test_min_dfa() -> None:
+    actual_dfa = get_min_dfa("i* l* y* a* | 1901")
+    expected_min_dfa = actual_dfa.minimize()
+
+    assert actual_dfa.is_equivalent_to(expected_min_dfa) and len(actual_dfa.states) == len(
+        expected_min_dfa.states
+    )
+
+
+@pytest.mark.parametrize(
+    "actual_regex, expected_word, not_expected_word",
+    [
+        ("", [], [Symbol("*")]),
+        (
+                "i* l* y* a* | 1901",
+                [Symbol("i"), Symbol("l"), Symbol("y"), Symbol("a")],
+                [Symbol("i"), Symbol("l"), Symbol("y"), Symbol("a"), Symbol("1901")],
+        ),
+        (
+                "(a | b)* 00* | 11*",
+                [Symbol("a"), Symbol("a"), Symbol("00"), Symbol("00"), Symbol("00")],
+                [Symbol("a"), Symbol("b"), Symbol("11")],
+        ),
+    ],
+)
+def test_min_dfa_accepts(
+        actual_regex: str,
+        expected_word: Iterable[Symbol],
+        not_expected_word: Iterable[Symbol],
+) -> None:
+    actual_min_dfa = get_min_dfa(actual_regex)
+
+    if actual_regex == "":
+        assert actual_min_dfa.is_empty()
+    else:
+        assert actual_min_dfa.accepts(expected_word) and not actual_min_dfa.accepts(
+            not_expected_word
+        )
+
+
+def test_get_min_dfa() -> None:
+    expected_min_dfa = DeterministicFiniteAutomaton()
+
+    state_0 = State(0)
+    state_1 = State(1)
+    state_2 = State(2)
+    state_3 = State(3)
+
+    symbol_i = Symbol("i")
+    symbol_l = Symbol("l")
+    symbol_y = Symbol("y")
+    symbol_a = Symbol("a")
+
+    expected_min_dfa.add_start_state(state_0)
+
+    expected_min_dfa.add_final_state(state_0)
+    expected_min_dfa.add_final_state(state_1)
+    expected_min_dfa.add_final_state(state_2)
+    expected_min_dfa.add_final_state(state_3)
+
+    expected_min_dfa.add_transition(state_0, symbol_i, state_0)
+    expected_min_dfa.add_transition(state_0, symbol_l, state_1)
+    expected_min_dfa.add_transition(state_0, symbol_y, state_2)
+    expected_min_dfa.add_transition(state_0, symbol_a, state_3)
+
+    expected_min_dfa.add_transition(state_1, symbol_l, state_1)
+    expected_min_dfa.add_transition(state_1, symbol_y, state_2)
+    expected_min_dfa.add_transition(state_1, symbol_a, state_3)
+
+    expected_min_dfa.add_transition(state_2, symbol_y, state_2)
+    expected_min_dfa.add_transition(state_2, symbol_a, state_3)
+
+    expected_min_dfa.add_transition(state_3, symbol_a, state_3)
+
+    actual_min_dfa = get_min_dfa("i* l* y* a*")
+
+    assert actual_min_dfa.is_equivalent_to(expected_min_dfa) and len(actual_min_dfa.states) == len(
+        expected_min_dfa.states
+    )
+
+
+@pytest.fixture
+def two_cycles_graph() -> Graph:
+    return get_two_cycles(2, 2)
+
+
+@pytest.fixture
+def expected_nfa() -> NondeterministicFiniteAutomaton:
+    nfa = NondeterministicFiniteAutomaton()
+    nfa.add_transitions([(0, 'a', 1), (1, 'a', 2), (2, 'a', 0), (0, 'b', 3), (3, 'b', 4), (4, 'b', 0)])
+
+    return nfa
+
+
+def test_wrong_states(two_cycles_graph) -> None:
+    with pytest.raises(ValueError):
+        get_nfa(two_cycles_graph.graph, {0, 4, 199}, {-9, 3, 19})
+
+
+def test_nfa(two_cycles_graph) -> None:
+    nfa = get_nfa(two_cycles_graph.graph)
+
+    assert not nfa.is_deterministic()
+
+
+@pytest.mark.parametrize(
+    "expected_word, not_expected_word",
+    [
+        ('', "epsilon"),
+        ("aaa", "bb"),
+        ("bbb", ' '),
+        ("bbbbbb", "aaabb"),
+        ("aaabbbaaa", 'b')
+    ]
+)
+def test_nfa_accepts(two_cycles_graph, expected_word, not_expected_word) -> None:
+    actual_nfa = get_nfa(two_cycles_graph.graph, {0}, {0})
+
+    assert actual_nfa.accepts(expected_word) and not actual_nfa.accepts(
+        not_expected_word
+    )
+
+
+@pytest.mark.parametrize(
+    "start_states, final_states",
+    [
+        ({0}, {3}),
+        ({0}, {1, 4}),
+        ({2, 3}, {0}),
+        ({0, 1, 2}, {0, 3, 4}),
+        (None, None)
+    ]
+)
+def test_get_nfa(two_cycles_graph, expected_nfa,
+                 start_states, final_states) -> None:
+    if not start_states:
+        start_states = {0, 1, 2, 3, 4}
+
+    for state in start_states:
+        expected_nfa.add_start_state(State(state))
+
+    if not final_states:
+        final_states = {0, 1, 2, 3, 4}
+
+    for state in final_states:
+        expected_nfa.add_final_state(State(state))
+
+    actual_nfa = get_nfa(two_cycles_graph.graph, start_states, final_states)
+
+    assert actual_nfa.is_equivalent_to(expected_nfa)
